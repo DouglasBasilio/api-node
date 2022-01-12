ba01955b70bd3b55b93b05eb173b8ef35473b659
Douglas Basilio <basilio.douglas@yahoo.com.br>
2022-01-11 22:32:15 -0300

    adicionando metodo GET

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 
 const ProductController = require('../controllers/products')
 
-router.get('/products', ProductController.get)
+router.get('/products/:id?', ProductController.get)
 // router.get('/products', ProductController.post)
 // router.get('/products/:id', ProductController.put)
 // router.get('/products/:id', ProductController.delete)
