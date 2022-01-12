8001d0862732463c7b044c85edb2116e76216481
Douglas Basilio <basilio.douglas@yahoo.com.br>
2022-01-12 14:51:36 -0300

    adicionando metodo DELETE

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -61,8 +61,32 @@ async function put(req, res){
 
 }
 
+async function remove(req, res){
+    // pegar o id
+    const { id } = req.params
+
+    const remove = await ProductsModel.deleteMany({ _id: id })
+
+    const message  = remove.ok ? 'success' : 'error'
+    
+    /*
+    let message = 'success'
+
+    if (!remove.ok) {
+        message = 'error'
+    }
+    */
+   
+    console.log(message)
+
+    res.send({
+        message,
+    })
+}
+
 module.exports = {
     get,
     post,
     put,
+    remove,
 }
